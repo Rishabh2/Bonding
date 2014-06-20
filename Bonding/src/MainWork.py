1be4ae2c3d7ecdc9f50276f7b6b027f5a9ecca20
virulentArachnid <sleedy13+git@gmail.com>
2014-06-20 16:12:00 -0400

    sync

--- Bonding/src/MainWork.py
+++ Bonding/src/MainWork.py
@@ -1,10 +1,12 @@
-#from Enemy import *
+from Enemy import *
 from Player import *
 from Ribbon import *
 from zellegraphics import *
+from random import *
 
 screenWidth = 1280
 screenHeight = 720
-
-win = GraphWin("RibBonding", screenWidth, screenHeight)
 Player.health = 100
+Enemy.health = randrange(1, 10)
+print(Enemy.health)
+win = GraphWin("RibBonding", screenWidth, screenHeight)
