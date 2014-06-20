671217d7b97026c1a2ae573d10cf9484ef7af88a
Rishabh <rishabh.bhasin.33@gmail.com>
2014-06-20 14:14:54 -0400

    Testing a commit

---
+++ Bonding/src/Player.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 20, 2014
+
+@author: Rishabh
+'''
+
+class Player(object):
+    '''
+    Class for the player objects
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
