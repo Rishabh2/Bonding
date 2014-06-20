95782ce07d125d4779c765b663404db9508ab06d
virulentArachnid <sleedy13+git@gmail.com>
2014-06-20 15:56:48 -0400

    nothing major

--- Bonding/src/MainWork.py
+++ Bonding/src/MainWork.py
@@ -1,8 +1,10 @@
 from Enemy import *
 from Player import *
 from Ribbon import *
+from zellegraphics import *
 
 screenWidth = 1280
 screenHeight = 720
 
 win = GraphWin("RibBonding", screenWidth, screenHeight)
+player.health = 100
