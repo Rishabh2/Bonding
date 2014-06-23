0a5eb89d1ed6ebd1835ebb9cfd13db81950c0fad
virulentArachnid <sleedy13+git@gmail.com>
2014-06-23 14:39:54 -0400

    lots of code

--- Bonding/src/Player.py
+++ Bonding/src/Player.py
@@ -2,16 +2,18 @@ class Player(object):
     '''
     Class for the player objects
     '''
-
+  
     
-    def __init__(self, health, speed, screenWidth, screenHeight):
+    def __init__(self, dx, dy, health, speed, screenWidth, screenHeight):
         self.health = health
         self.speed = speed
         self.playerPoint = (screenWidth//2 + 50, screenHeight//2 + 75/2)
+        self.dx = dx
+        self.dy = dy
         
     def draw(self, surface):
         self.draw(screen)
     
     def move(self, dx, dy):
-        playerPoint = (MainWork.playerPoint[0]+dx, MainWork.point[1]+dy)
-        return playerPoint
+        self.playerPoint = (self.playerPoint[0] + dx, self.playerPoint[1] + dy)
+        return self.playerPoint
