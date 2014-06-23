d152e5e11564f75ec81a81f1965e58cfdba6d57c
virulentArachnid <sleedy13+git@gmail.com>
2014-06-23 13:46:41 -0400

    vars

--- Bonding/src/MainWork.py
+++ Bonding/src/MainWork.py
@@ -5,11 +5,11 @@ import random
 import Floor
 import Room
 import pygame as pyg
+from pygame.locals import *
 pyg.init()
 
 screenWidth = 1280
 screenHeight = 720
-Player.health = 100
 FPS = 60
 roomRows = 10
 roomCols = 10
@@ -21,10 +21,14 @@ totalheight = tileSize * tileRows
 xoffset = (screenWidth - totalwidth) / 2
 yoffset = (screenHeight - totalheight) / 2
 tileImage = pyg.image.load("Tile.png")
+playerImage = pyg.image.load("Player.png")
 
 screen = pyg.display.set_mode([screenWidth, screenHeight])
 pyg.display.set_caption("Map Test")
 floor = Floor.Floor(roomRows, roomCols, tileRows, tileCols)
+playerPoint = (screenWidth//2 + 50, screenHeight//2 + 75/2)
+screen.blit(playerImage, (playerPoint[0] - 50, playerPoint[1] - 72/2))
+player = Player.Player(100, 1, screenWidth, screenHeight)
 
 def drawBorder():
     pyg.draw.rect(screen, pyg.Color("white"), pyg.Rect(xoffset, yoffset, totalwidth, totalheight))
@@ -40,6 +44,7 @@ def mainLoop():
         for row in range(tileRows):
             for col in range(tileCols):
                 screen.blit(tileImage, [xoffset + col * tileSize, yoffset + row * tileSize])
+        screen.blit(playerImage, (player.playerPoint[0] - 50, player.playerPoint[1] - 72/2))
         pyg.display.update()
     
 mainLoop()
--- Bonding/src/Player.py
+++ Bonding/src/Player.py
@@ -1,14 +1,17 @@
-'''
-Created on Jun 20, 2014
-
-@author: Rishabh
-'''
-
 class Player(object):
     '''
     Class for the player objects
     '''
 
-
-    def __init__(self, health):
+    
+    def __init__(self, health, speed, screenWidth, screenHeight):
         self.health = health
+        self.speed = speed
+        self.playerPoint = (screenWidth//2 + 50, screenHeight//2 + 75/2)
+        
+    def draw(self, surface):
+        self.draw(screen)
+    
+    def move(self, dx, dy):
+        playerPoint = (MainWork.playerPoint[0]+dx, MainWork.point[1]+dy)
+        return playerPoint
