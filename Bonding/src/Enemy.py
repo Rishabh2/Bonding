6718aa2feaeb620e2927b3cd47d0e94693f22642
virulentArachnid <sleedy13+git@gmail.com>
2014-06-23 10:41:35 -0400

    scaffold for AI

--- Bonding/src/Enemy.py
+++ Bonding/src/Enemy.py
@@ -1,4 +1,6 @@
 import player
+from math import *
+
 class Enemy(object):
     def __init__(self, health, damageDealer, speed):
         '''
@@ -7,5 +9,13 @@ class Enemy(object):
         self.health = health
         self.damage = damageDealer
         self.speed = speed
-    def _move(self, speed):
-        pass
+    def getDistance(p1, p2):
+        return sqrt(((p2.getX() - p1.getX()) ** 2) + ((p2.getY() - p1.getY()) ** 2))
+    
+    def move(self, speed, player1, player2):
+        distanceToP1 = getDistance(self, player1)
+        distanceToP2 = getDistance(self, player2)
+        if distanceToP1 < distanceToP2:
+            pass
+        else:
+            pass    
