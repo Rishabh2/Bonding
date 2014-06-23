3671d6c33be78f8f94684e7ddf71110e1802a249
Rishabh <rishabh.bhasin.33@gmail.com>
2014-06-23 19:18:04 -0400

    Finally finished fucking floor

--- Bonding/src/Floor.py
+++ Bonding/src/Floor.py
@@ -1,39 +1,45 @@
 import Room
 import random
+import queue
+
+class ListQueue(queue.PriorityQueue):
+    # @overrides(queue.PriorityQueue)
+    def __cmp__(self, other):
+        return len(self) - len(other)
     
 class Floor(object):
     
     def longestPath(self, current, path):
         neighbors = []
-        paths = []
+        paths = ListQueue()
         doors = self.rooms[current[0]][current[1]].doors
         if doors[0]:
             if current[0] > 0:
-                if not rooms[current[0] - 1][current[1]] in path:
-                    neighbors.append([current[0] - 1][current[1]])
+                    neighbors.append((current[0] - 1, current[1]))
         if doors[1]:
-            if current[1] < len(rooms[0]) - 1:
-                if not rooms[current[0]][current[1] + 1] in path:
-                    neighbors.append([current[0]][current[1] + 1])
+            if current[1] < len(self.rooms[0]) - 1:
+                    neighbors.append((current[0], current[1] + 1))
         if doors[2]:
-            if current[0] < len(rooms[0]) - 1:
-                if not rooms[current[0] + 1][current[1]] in path:
-                    neighbors.append([current[0] + 1][current[1]])
+            if current[0] < len(self.rooms[0]) - 1:
+                    neighbors.append((current[0] + 1, current[1]))
         if doors[3]:
             if current[1] > 0:
-                if not rooms[current[0]][current[1] - 1] in path:
-                    neighbors.append([current[0]][current[1] - 1])
+                    neighbors.append((current[0], current[1] - 1))
+        for neighbor in neighbors:
+            if neighbor in path:
+                neighbors.remove(neighbor)
         if len(neighbors) == 0:
             return path
         else:
             for neighbor in neighbors:
-                paths.append(longestPath(neighbor, path + neighbor))
-        return paths[maxlen(paths)]
+                path.append(neighbor)
+                paths.put(self.longestPath(neighbor, path))
+        return paths.get()
 
     def __init__(self, rows, cols, tileRows, tileCols):
         self.rooms = [[Room.Room(row, col, tileRows, tileCols, [[0 for col in range(tileCols)] for row in range(tileRows)]) for col in range(cols)] for row in range(rows)]
-        self.currentRoom = [0, 0]
-        current = [0, 0]
+        self.currentRoom = (0, 0)
+        current = (0, 0)
         roomStack = []
         totalRooms = rows * cols
         visitedRooms = 1
@@ -61,15 +67,7 @@ class Floor(object):
             else:
                 current = roomStack.pop()
         
-        
-        self.bossRoom = self.longestPath([0, 0], [[0, 0]])[0]
+        self.bossRoom = self.longestPath((0, 0), [(0, 0)])[-1]
                 
-    def maxlen(self, lists):
-        maxLen = 0
-        for i in range(len(lists)):
-            if len(lists[i]) > maxLen:
-                maxLen = len(lists[i])
-                maxIndex = i
-        return maxIndex
         
 
