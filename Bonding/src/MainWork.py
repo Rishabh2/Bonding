5dccd3535d33820d53f3e26ffaaf360d263cf19d
Rishabh <rishabh.bhasin.33@gmail.com>
2014-06-24 14:37:27 -0400

    Made Borders

--- Bonding/src/MainWork.py
+++ Bonding/src/MainWork.py
@@ -16,13 +16,17 @@ screenHeight = 800
 FPS = 60
 roomRows = 10
 roomCols = 10
-tileRows = 8
-tileCols = 15
+tileRows = 6
+tileCols = 13
 tileSize = 75
+totalRows = 8
+totalCols = 15
 playerHeight = 75
 playerWidth = 50
-totalwidth = tileSize * tileCols
-totalheight = tileSize * tileRows
+tileWidth = tileSize * tileCols
+tileHeight = tileSize * tileRows
+totalwidth = tileSize * totalCols
+totalheight = tileSize * totalRows
 xoffset = (screenWidth - totalwidth) / 2
 yoffset = (screenHeight - totalheight) * 3 / 4
 mapSize = 120
@@ -31,6 +35,12 @@ mapBuffer = 10
 tileImage = pyg.image.load("Tile.png")
 playerImage = pyg.image.load("Player.png")
 player2Image = pyg.image.load("Player.png")
+cornerImage = pyg.image.load("Corner.png")
+cornerImage2 = pyg.image.load("Corner2.png")
+cornerImage3 = pyg.image.load("Corner3.png")
+cornerImage4 = pyg.image.load("Corner4.png")
+wallImage = pyg.image.load("Wall.png")
+vwallImage = pyg.image.load("VWall.png")
 screen = pyg.display.set_mode([screenWidth, screenHeight])
 pyg.display.set_caption("Map Test")
 floor = Floor.Floor(roomRows, roomCols, tileRows, tileCols)
@@ -64,7 +74,17 @@ def mainLoop():
         drawBorder()
         for row in range(tileRows):
             for col in range(tileCols):
-                screen.blit(tileImage, [xoffset + col * tileSize, yoffset + row * tileSize])
+                screen.blit(tileImage, (xoffset + (col + 1) * tileSize, yoffset + (row + 1) * tileSize))
+        screen.blit(cornerImage, (xoffset, yoffset))
+        screen.blit(cornerImage2, (xoffset + tileSize + tileWidth, yoffset))
+        screen.blit(cornerImage3, (xoffset + tileSize + tileWidth, yoffset + tileSize + tileHeight))
+        screen.blit(cornerImage4, (xoffset, yoffset + tileSize + tileHeight))
+        for i in range(tileCols):
+            screen.blit(wallImage, (xoffset + tileSize * (i + 1), yoffset))
+            screen.blit(wallImage, (xoffset + tileSize * (i + 1), yoffset + tileSize + tileHeight))
+        for i in range(tileRows):
+            screen.blit(vwallImage, (xoffset, yoffset + tileSize * (i + 1)))
+            screen.blit(vwallImage, (xoffset + tileSize + tileWidth, yoffset + tileSize * (i + 1)))
         screen.blit(playerImage, (player.playerPoint[0] - playerWidth / 2, player.playerPoint[1] - playerHeight / 2))
         screen.blit(player2Image, (player2.playerPoint[0] - playerWidth / 2, player2.playerPoint[1] - playerHeight / 2))
         if player.living == True:
@@ -83,13 +103,13 @@ def mainLoop():
                 player.move(-1 * player.speed, 0)
             if (pyg.key.get_pressed()[K_d]):
                 player.move(1 * player.speed, 0)
-            while player.playerPoint[0] - playerWidth / 2 < xoffset:
+            while player.playerPoint[0] - playerWidth / 2 < xoffset + tileSize:
                 player.move(1, 0)
-            while player.playerPoint[0] + playerWidth / 2 > totalwidth + xoffset:
+            while player.playerPoint[0] + playerWidth / 2 > xoffset + tileSize + tileWidth:
                 player.move(-1, 0)
-            while player.playerPoint[1] - playerHeight / 2 < yoffset:
+            while player.playerPoint[1] - playerHeight / 2 < yoffset + tileSize:
                 player .move(0, 1)
-            while player.playerPoint[1] + playerHeight / 2 > totalheight + yoffset:
+            while player.playerPoint[1] + playerHeight / 2 > yoffset + tileSize + tileHeight:
                 player.move(0, -1)
         else:
             player.playerPoint(screenWidth * 1 / 4, screenHeight)
@@ -105,13 +125,13 @@ def mainLoop():
                 player2.move(-1 * player2.speed, 0)
             if (pyg.key.get_pressed()[K_RIGHT]):
                 player2.move(1 * player2.speed, 0)
-            while player2.playerPoint[0] - playerWidth / 2 < xoffset:
+            while player2.playerPoint[0] - playerWidth / 2 < xoffset + tileSize:
                 player2.move(1, 0)
-            while player2.playerPoint[0] + playerWidth / 2 > totalwidth + xoffset:
+            while player2.playerPoint[0] + playerWidth / 2 > xoffset + tileSize + tileWidth:
                 player2.move(-1, 0)
-            while player2.playerPoint[1] - playerHeight / 2 < yoffset:
+            while player2.playerPoint[1] - playerHeight / 2 < yoffset + tileSize:
                 player2.move(0, 1)
-            while player2.playerPoint[1] + playerHeight / 2 > totalheight + yoffset:
+            while player2.playerPoint[1] + playerHeight / 2 > yoffset + tileSize + tileHeight:
                 player2.move(0, -1)
         else:
             player2.playerPoint(screenWidth * 3 / 4, screenHeight)
